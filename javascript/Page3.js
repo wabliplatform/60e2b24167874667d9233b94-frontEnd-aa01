9d9afdaf33a9c974bf0e390dbe701d168039b327
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2021-12-15 15:05:32 +0200

    My commit message

--- javascript/Page3.js
+++ javascript/Page3.js
@@ -12,7 +12,12 @@ let apiBlogApi = new TempApi.BlogApi();import TempApi from '../src/index';window
         if(insideSubDataElement !== null){
           insideSubDataElement.textContent = data[i].stext;
         }
+       } catch (e) { console.log(e) };try { 
+        const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'deletepost']");
+        if(insideSubDataElement !== null){
+          insideSubDataElement.undefined = data[i].deletepost;
+        }
        } catch (e) { console.log(e) };
       }
     });
-    [...subDataElements].forEach((element,index) => {if(index > data.length) subDataElements[index].remove()})}});};
+    [...subDataElements].forEach((element,index) => {if(index > data.length) subDataElements[index].remove()})}});};document.getElementById('idmz4').onclick = () => {let blogId = window.location.pathname.replace('/Page3/','');apiBlogApi.deleteblog( blogId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};
